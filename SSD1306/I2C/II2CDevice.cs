a2ce35d30fa5e6e76fb8e6969c4cedac0abb58fb
Stuart Johnson <stuart@factoid.org>
2019-12-22 15:47:45 +0000

    missing files

---
+++ SSD1306/I2C/II2CDevice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSD1306.I2C
+{
+    public interface II2CDevice
+    {
+
+        void WriteBytes(byte regAddress, byte[] data);
+        void WriteByte(byte regAddress, byte data);
+    }
+}
